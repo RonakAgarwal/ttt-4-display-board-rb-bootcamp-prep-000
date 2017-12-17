fe05af1d434c9635a88e3515dd8eb7e4b5cc378b
RonakAgarwal <agarwalronak02@gmail.com>
2017-12-17 19:42:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,10 @@
   def display_board(display) 
     puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
     puts "-----------"
+    puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
+    
+  end
+
+board = 
